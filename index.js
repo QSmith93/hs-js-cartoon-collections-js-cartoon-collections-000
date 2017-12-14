9644761b2a31811e92ed9f7412dc7978d0429c1a
Quentin <q.smith1@yahoo.com>
2017-12-14 16:31:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 function dwarfRollCall(dwarves) {
+  dwarves = [];
+  for (var i = 0; i < dwarves.length; i++) {
+    var call = []
+    call.push((i+1) + ". " + dwarves[i])
+  }
+  return call.join(" ")
 }
 
 function summonCaptainPlanet(planeteerCalls){
