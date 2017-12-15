13c7c53c1ff3d851ba3bdcf8db8b537f506e22cf
Quentin <q.smith1@yahoo.com>
2017-12-15 03:56:55 +0000

    Done.

--- index.js
+++ index.js
@@ -4,11 +4,6 @@ function dwarfRollCall(dwarves) {
     call.push((i+1) + ". " + dwarves[i])
   }
   return  call.join(" ")
-  dwarves = ["Dopey", "Grumpy", "Bashful"];
-  for (i = 0; i < dwarves.length; i++) {
-  dwarves.push((i+1) + ". " + dwarves[i])
-  }
-  return 
 }
 // I don't know why my result is being rejected. I have the exact result expected.
 
@@ -29,6 +24,7 @@ function longPlaneteerCalls(words) {
     }
   }
 }
+// Having trouble best understanding loop conditionals.
 // I know that my function is assessing the length of the first element of the index, but I don't believe that it is continuing to assess down the line. How can I change my FOR loop to properly search this? 
 // Or is my function so far from what was expected? Am I supposed to set up a function to check shortCalls vs longCalls?
 
