9b89fc576743201cd1be24f1ec57dc3998f6b6b3
Quentin <q.smith1@yahoo.com>
2017-12-15 02:48:11 +0000

    Done.

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ function dwarfRollCall(dwarves) {
     call.push((i+1) + ". " + dwarves[i])
   }
   return  call.join(" ")
+  dwarves = ["Dopey", "Grumpy", "Bashful"];
+  for (var i = 0; i < dwarves.length; i++) {
+  dwarves.push((i+1) + ". " + dwarves[i])
+  }
+  return 
 }
 
 function summonCaptainPlanet(planeteerCalls) {
@@ -14,6 +19,14 @@ function summonCaptainPlanet(planeteerCalls) {
 }
 
 function longPlaneteerCalls(words) {
+  for (var i = 0; i < words.length; i++) {
+    var n = words[i].length
+    if (n > 4) {
+      return "True"
+    } else {
+    return "False"
+    }
+  }
 }
 
 function findTheCheese (foods) {
